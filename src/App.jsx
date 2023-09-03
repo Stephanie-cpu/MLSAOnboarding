ee701ca290c846091ce491e496ac52e142f92986
Egharevba Stephanie Osasumwen <115887454+Stephanie-cpu@users.noreply.github.com>
2023-09-03 18:23:24 +0000

    img

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
-import Education from "./Components/Education";
+
 import "./styles.css";
 
 /**
@@ -44,7 +44,7 @@ const App = () => {
       <Home name={siteProps.EgharevbaStephanie} title={siteProps.SefiniStephanie} />
       <About />
       <Portfolio />
-      <Education />
+
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
